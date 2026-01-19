0b0264ffed0acfe7eed1af274f3b225f906c9b6b
Mahalakshmi <136691991+Mahalakshmibsc@users.noreply.github.com>
2026-01-19 12:50:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function contactMe() {
-    alert("You can contact me via WhatsApp or Email!");
+function openWhatsApp() {
+    var phoneNumber = "919080451235"; // Your number with country code
+    var message = "Hello Maha Lakshmi, I viewed your portfolio and would like to connect with you.";
+
+    var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank");
+}
+
+function sendEmail() {
+    window.location.href = "mailto:mahalakshmi.bsc23@gmail.com?subject=Portfolio Review&body=Hello Maha Lakshmi,";
 }
